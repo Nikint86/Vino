d561f8d7e8d3d03c353f792a6afeab80b8001178
Nikint86 <lobackiy_2007@mail.ru>
2026-02-16 13:19:07 +0300

    Использовал defaultdict

--- parse_excel.py
+++ parse_excel.py
@@ -1,14 +1,12 @@
+from collections import defaultdict
 import pandas as pd
 from pprint import pprint
-import math
 
 df = pd.read_excel('wine2.xlsx', sheet_name='Лист1')
 
-products = {}
+products = defaultdict(list)
 
 for index, row in df.iterrows():
-    category = row['Категория']
-
     product = {
         'Название': row['Название'],
         'Сорт': row['Сорт'] if pd.notna(row['Сорт']) else '',
@@ -16,11 +14,8 @@ for index, row in df.iterrows():
         'Картинка': row['Картинка']
     }
 
-    if category not in products:
-        products[category] = []
+    products[row['Категория']].append(product)
 
-    products[category].append(product)
-
-
+print("Продукция на винодельне Розы:")
 print("=" * 50)
-pprint(products)
+pprint(dict(products)) 
